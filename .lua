bc2555e023ad48cbad02ada8dd310aec0f5b55c5
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-12-28 18:18:38 +0900

    Update .lua

--- .lua
+++ .lua
@@ -3,6 +3,24 @@ local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub", HidePremium = false
 local workspace = game:GetService("Workspace")
 local event = game:GetService("ReplicatedStorage")["Events"]
 
+local args = {
+    [1] = "{edcd8178-23dd-4621-94b4-8ade0f639ac1}",
+    [2] = workspace.Bombs.Normal,
+    [3] = Vector3.new(50.048213958740234, -44.50149917602539, -174.61976623535156)
+}
+
+--game:GetService("ReplicatedStorage").Events.ProjectileHit:FireServer(unpack(args))
+
+local args = {
+    [1] = "Sword",
+    [2] = {
+        ["name"] = "Wooden",
+        ["hit"] = workspace.Bombs.Normal
+    }
+}
+
+--game:GetService("ReplicatedStorage").Events.WeaponAction:FireServer(unpack(args))
+
 local T1 = Window:MakeTab({
    Name = "Main",
    Icon = "rbxassetid://",
@@ -16,14 +34,14 @@ local T2 = Window:MakeTab({
 })
 
 T1:AddToggle({
-  Name = "Instant kill",
+  Name = "Instant kill ( use wooden sword )",
   Default = false,
   Callback = function(Value)
      _G.bomb = Value
       while wait() do
         if _G.bomb == false then break end
         for i,v in pairs(workspace["Bombs"]:GetChildren()) do
-          event["WeaponAction"]:FireServer("Sword",{"Wooden",v})
+          event["WeaponAction"]:FireServer("Sword",{["name"] = "Wooden",["hit"] = v})
         end
       end
   end    
